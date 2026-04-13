d4522fef7bdcea6efd332c111e7e689e318af66e
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-04-13 14:44:19 +0200

    .

--- essaie.py
+++ essaie.py
@@ -1,6 +1,20 @@
-a="111DD"
+import string
 
-if a.isdigit() or a.isalpha():
-    print("ok")
-else: 
-    print ("non")
+def generer_password_lisible(longueur=12):
+    
+    ambigus = "ilI1Lo0O"
+    
+    lettres = "".join(c for c in string.ascii_letters if c not in ambigus)
+    chiffres = "".join(c for c in string.digits if c not in ambigus)
+    
+    speciaux = "!@#$%" 
+    
+    source = lettres + chiffres + speciaux
+    
+    
+    password = ''.join(secrets.choice(source) for _ in range(longueur))
+    return password
+
+
+mon_password = generer_password_lisible(10)
+print(mon_password)
